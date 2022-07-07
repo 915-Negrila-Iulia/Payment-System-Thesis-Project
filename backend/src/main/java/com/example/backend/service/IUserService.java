0f577597032a4a4561d82927f87e2915642d59fe
915-Negrila-Iulia <iulia_n06@yahoo.ro>
2022-07-07 22:49:13 +0300

    used BCryptPasswordEncoder from Spring Security to encode user password before storing it in the database

--- backend/src/main/java/com/example/backend/service/IUserService.java
+++ backend/src/main/java/com/example/backend/service/IUserService.java
@@ -1,6 +1,7 @@
 package com.example.backend.service;
 
 import com.example.backend.model.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,6 +10,6 @@ public interface IUserService {
     User saveUser(User user);
     void deleteUserById(Long id);
     Optional<User> findUserById(Long id);
-    User login(String username, String password);
+    User findUserByUsername(String username);
     List<User> getAllUsers();
 }
