5deba2b190058ffc1051e15682ede2b05fd1215f
915-Negrila-Iulia <iulia_n06@yahoo.ro>
2022-07-11 23:43:30 +0300

    crud functionalities on Account entity and history of accounts- backend done

---
+++ backend/src/main/java/com/example/backend/model/AccountStatusEnum.java
@@ -0,0 +1,9 @@
+package com.example.backend.model;
+
+public enum AccountStatusEnum {
+    OPEN,
+    BLOCKED,
+    BLOCK_DEBIT,
+    BLOCK_CREDIT,
+    CLOSED
+}
---
+++ backend/src/main/java/com/example/backend/service/IAccountService.java
@@ -0,0 +1,12 @@
+package com.example.backend.service;
+
+import com.example.backend.model.Account;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IAccountService {
+    Account saveAccount(Account account);
+    Optional<Account> findAccountById(Long id);
+    List<Account> getAllAccounts();
+}
