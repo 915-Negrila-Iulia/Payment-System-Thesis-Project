303333abcc47a2c34cdac1df27e22514820a960d
915-Negrila-Iulia <iulia_n06@yahoo.ro>
2022-07-12 10:45:39 +0300

    show list, update/approve -account done

--- frontend/src/app/accounts-list/accounts-list.component.ts
+++ frontend/src/app/accounts-list/accounts-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Account } from '../account';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-accounts-list',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountsListComponent implements OnInit {
 
-  constructor() { }
+  accounts: Account[] = [];
+  account: Account = new Account();
+
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.getAccounts();
+  }
+
+  getAccounts(){
+    this.accountService.getAllAccounts().subscribe(data => {
+      this.accounts = data;
+    })
+  }
+
+  updateAccount(id: any, iban: any, countryCode: any, bankCode: any, currency: any, accountStatus: any, personID: any, status: any){
+    this.account.id = id;
+    this.account.iban = iban.value;
+    this.account.countryCode = countryCode.value;
+    this.account.bankCode = bankCode.value;
+    this.account.currency = currency.value;
+    this.account.accountStatus = accountStatus.value;
+    this.account.personID = personID;
+    this.account.status = status;
+    this.accountService.updateAccount(id,this.account).subscribe(data => {
+      console.log(data);
+    },
+    error => console.log(error)
+    );
+  }
+
+  approveAccount(id: any){
+    this.accountService.approveAccount(id).subscribe(data => {
+      console.log(data);
+    },
+    error => console.log(error)
+    );
   }
 
 }
